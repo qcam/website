6889d0c85f3d1c419200d8b526742b57178b5897
Cẩm Huỳnh <huynhquancam@gmail.com>
2020-07-06 22:53:48 +0200

    Upgrade Saxy and Fiet

--- mix.exs
+++ mix.exs
@@ -27,8 +27,8 @@ defmodule HQC.Mixfile do
       {:cowboy, "~> 1.1.2"},
       {:plug, "~> 1.3.4"},
       {:hackney, "~> 1.15.0"},
-      {:fiet, github: "qcam/fiet"},
-      {:saxy, github: "qcam/saxy", override: true},
+      {:fiet, "~> 0.3"},
+      {:saxy, "~> 1.2"},
       {:nimble_parsec, "~> 0.3.2", runtime: false}
     ]
   end
